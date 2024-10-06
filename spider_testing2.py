eb0def6889f47233c95ad10e4c9789f492dc12c1
Mikelfincas24 <mikellopezfincas24@gmail.com>
2024-10-06 19:55:25 +0200

    Update spider_testing2.py

--- spider_testing2.py
+++ spider_testing2.py
@@ -7,7 +7,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import json
 import time
-import streamlit as st
+
 
 
 
@@ -51,7 +51,7 @@ class AsyncSpiderFunctions:
 
         browser.select_form('form[action="/search"]')
 
-        browser["q"] = str(value)
+        browser["q"] = value
 
         browser.submit_selected()
         # ---------------------------------------------------
@@ -63,9 +63,7 @@ class AsyncSpiderFunctions:
         if emails:
             for email in emails:
                 data_json[str(indice)] = email
-                st.write(emails)
-                   
-                # st.write(f"Email encontrado: {email}, Valor: {value}")
+                print(email,value)
                 return email
 
         # --------------------------------------
@@ -83,7 +81,7 @@ class AsyncSpiderFunctions:
                 email_page = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', soup_data.text)
                 data_json[str(indice)] = email_page[0]
                 
-                st.write(email_page[0],"---------",value)
+                print(email_page[0],"---------",value)
             except Exception as e:
                 print(f"Error: {e}")
 
@@ -91,7 +89,8 @@ class AsyncSpiderFunctions:
     async def httpx_http(self,dataframe):
 
         data_json = {}
-
+        
+        
         df = pd.read_excel(dataframe)
 
         df_final = df.copy()
@@ -118,11 +117,11 @@ class AsyncSpiderFunctions:
             values_xp = dict(list(filter(lambda x: "axesor" not in x[1] and "einforma" not in x[1] ,other_value)))
             df_final["Indice"] = list(range(0,df_final.shape[0]))
             df_final["Indice"] = df_final["Indice"].astype(str)
-            df_final["New_email"] = df_final.apply(insert_valores_json,axis=1,args=(data_json,))
+            df_final["New_email"] = df_final.apply(insert_valores_json,axis=1, args=(data_json,))
             df_final.drop(columns=["EMAIL","Indice"],inplace=True)
             print(data_json)
 
-            return df_final,data_json
+            return df_final
 
             # return data_json
             # return results
@@ -143,4 +142,3 @@ class AsyncSpiderFunctions:
     # with open(r"C:\Users\Cash\Proyectos\092024\Multiples administradores fincas web scrapping\values.json","w") as j:
     #     json.dump(data_json,j)
     
-
